5879990689e9e624274806b07cb3f2b287aa3ff4
raulgarcia <garcia.raul@gmail.com>
2020-09-21 18:57:45 -0400

    Storyboard connected to ViewController

--- RxSwiftMVVMLoginValidator/ViewController.swift
+++ RxSwiftMVVMLoginValidator/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var loginEnabledLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
